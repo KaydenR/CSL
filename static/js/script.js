abbd88753c9385272ad426ca33d5a5406cfe8738
KaydenR <Kaydenrioslbc@gmail.com>
2022-08-07 22:00:25 -0700

    Auto scrolling added, JS file renamed

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,15 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("scrolling-active", windowPosition);
 })
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
